8d2af0c58ac251989ed7abc8e1cca255170817f6
Florient AKOUDAD <floakoud@e3r10p5.42.fr>
2019-06-11 13:38:46 +0200

    add exit window

---
+++ main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: floakoud <floakoud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/09 13:23:04 by floakoud          #+#    #+#             */
+/*   Updated: 2019/06/11 13:29:57 by floakoud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include "fdf.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void    ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int     deal_key(int key, void *param)
+{
+	(void)param;
+	if (key == 53)
+		exit(0);
+	return(0);
+}
+
+int     main()
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Fdf");
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 200, 0xFFFFFF);
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
+	mlx_loop(mlx_ptr);
+	return (0);
+}
